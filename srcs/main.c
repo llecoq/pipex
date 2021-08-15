664664c879e39476df2173b49eb878ec9a3c067d
llecoq <llecoq@student.42lyon.fr>
2021-08-15 17:16:16 +0200

    fixed segfault if pipex is not initiated

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: llecoq <llecoq@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/08 10:15:32 by user42            #+#    #+#             */
-/*   Updated: 2021/08/15 16:08:24 by llecoq           ###   ########.fr       */
+/*   Updated: 2021/08/15 17:14:08 by llecoq           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,10 +26,10 @@ int	main(int argc, char **argv, char **envp)
 	t_pipe	pipex;
 	int		child_status;
 
+	init_pipex(&pipex, envp);
 	if ((argc < 5 && argc > 2) || argc < 2)
 		error_quit(&pipex, "not enough arguments\nusage :  ./pipex \
 file1 cmd1 cmd2 cmd3 ... cmdn file2", -1);
-	init_pipex(&pipex, envp);
 	parse(&pipex, argv, argc);
 	child_status = evaluator(&pipex, pipex.cmds, pipex.cmds_nb);
 	clear_memory(&pipex);
